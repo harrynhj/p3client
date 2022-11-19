fa46640962d2b3a98e78d2b5a2672ab866b6aa81
vmuradov <43888259+vmuradov@users.noreply.github.com>
2022-11-19 15:32:24 -0600

    fixed

--- p3client-master/src/main/java/ClientGUI.java
+++ p3client-master/src/main/java/ClientGUI.java
@@ -19,20 +19,8 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class ClientGUI extends Application {
-
-	
-	// TextField s1,s2,s3,s4, c1;
-	// Button serverChoice,clientChoice,b1;
-	// HashMap<String, Scene> sceneMap;
-	// GridPane grid;
-	// HBox buttonBox;
-	// VBox clientBox;
-	// Scene startScene;
-	// BorderPane startPane;
-	// Server serverConnection;
 	Client clientConnection;
 	
-	// ListView<String> listItems, listItems2;
 	TextField ip, port;
     Label welcome, sep;
     Button quit, connect;
@@ -99,75 +87,5 @@ public class ClientGUI extends Application {
         primaryStage.setScene(scene);
 		primaryStage.show();
 	};
-		
-		
-	// 	this.clientChoice = new Button("Client");
-	// 	this.clientChoice.setStyle("-fx-pref-width: 300px");
-	// 	this.clientChoice.setStyle("-fx-pref-height: 300px");
-		
-		// this.clientChoice.setOnAction(e-> {primaryStage.setScene(sceneMap.get("client"));
-		// 									primaryStage.setTitle("This is a client");
-		// 									clientConnection = new Client(data->{
-		// 					Platform.runLater(()->{listItems2.getItems().add(data.toString());
-		// 									});
-		// 					});
-							
-	// 										clientConnection.start();
-	// 	});
-		
-	// 	this.buttonBox = new HBox(400, serverChoice, clientChoice);
-	// 	startPane = new BorderPane();
-	// 	startPane.setPadding(new Insets(70));
-	// 	startPane.setCenter(buttonBox);
-		
-	// 	startScene = new Scene(startPane, 800,800);
-		
-	// 	listItems = new ListView<String>();
-	// 	listItems2 = new ListView<String>();
-		
-	// 	c1 = new TextField();
-	// 	b1 = new Button("Send");
-	// 	b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
-		
-	// 	sceneMap = new HashMap<String, Scene>();
-		
-	// 	sceneMap.put("server",  createServerGui());
-	// 	sceneMap.put("client",  createClientGui());
-		
-	// 	primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-    //         @Override
-    //         public void handle(WindowEvent t) {
-    //             Platform.exit();
-    //             System.exit(0);
-    //         }
-    //     });
-		
-		 
-		
-	// 	primaryStage.setScene(startScene);
-	// 	primaryStage.show();
-		
-	// }
-	
-	// public Scene createServerGui() {
-		
-	// 	BorderPane pane = new BorderPane();
-	// 	pane.setPadding(new Insets(70));
-	// 	pane.setStyle("-fx-background-color: coral");
-		
-	// 	pane.setCenter(listItems);
-	
-	// 	return new Scene(pane, 500, 400);
-		
-		
-	// }
-	
-	// public Scene createClientGui() {
-		
-	// 	clientBox = new VBox(10, c1,b1,listItems2);
-	// 	clientBox.setStyle("-fx-background-color: blue");
-	// 	return new Scene(clientBox, 400, 300);
-		
-	// }
 
 }
